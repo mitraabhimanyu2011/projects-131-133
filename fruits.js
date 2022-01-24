a33609310e4835958695e26d90f9d83ba611c179
mitraabhimanyu2011 <82744734+mitraabhimanyu2011@users.noreply.github.com>
2022-01-23 17:59:59 -0800

    Add files via upload

--- fruits.js
+++ fruits.js
@@ -1,8 +1,10 @@
+img = "";
 rank = "";
+objects = [];
 
 function preload()
 {
-    loadImage("king_bed.jpg");
+    img = loadImage("room.jpg");
 }
 
 function setup()
@@ -14,7 +16,21 @@ function setup()
 
 function draw()
 {
+    image(img, 0, 0, 640, 420);
 
+    if(rank != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects have been DETECTED!";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x - 50, objects[i].y - 150);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x - 50, objects[i].y - 150, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function modelLoaded()
@@ -24,11 +40,12 @@ function modelLoaded()
     objectDetector.detect(img, gotResults);
 }
 
-function gotResults()
+function gotResults(error, results)
 {
     if (error)
     {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
